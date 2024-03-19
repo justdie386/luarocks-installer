7d35f91fb91510965d8084cad16cb6d3c346296e
Bouchard Justin <79466202+justdie386@users.noreply.github.com>
2024-03-19 11:01:18 -0400

    Fix lua mismatch version
    Updated to properly install lua5.1, and lua5.1 only, as it would fetch lua5.4 files and lua5.1 include files

--- installer/main.lua
+++ installer/main.lua
@@ -1,30 +1,29 @@
---THIS IS A PROTOTYPE, MEANT TO BE AN EXAMPLE, I WILL EDIT THE THINGS TO MAKE IT ACTUALLY INSTALL LUA PROPERLY AND SET IT UP WITH LUAROCKS, AND THE PATH/CPATH
-package.path = "./?.lua;"..package.path
+package.path = "./src/?.lua;./?.lua;"..package.path
 local installer = require("install")
 local AppData = os.getenv("APPDATA")
 local files = {
   lua_exec = {
     name = "lua_exec.zip",
-    url = "https://pilotfiber.dl.sourceforge.net/project/luabinaries/5.4.2/Tools%20Executables/lua-5.4.2_Win64_bin.zip",
+    url = "https://netactuate.dl.sourceforge.net/project/luabinaries/5.1.5/Tools%20Executables/lua-5.1.5_Win64_bin.zip",
     unzip = true,
-    files = { --files that will be unzipped from the lua_exec.zip
-      { "lua54.exe", AppData..[[\lua\bin\lua54.exe]] },
-      { "lua54.dll", AppData..[[\lua\bin\lua54.dll]] },
+    files = {
+      { "lua5.1.exe", AppData..[[\lua\bin\lua51.exe]] },
+      { "lua5.1.dll", AppData..[[\lua\bin\lua51.dll]] },
     },
-    directories = { --directories that will be created, for your files to be placed
+    directories = {
       AppData..[[\lua]],
       AppData..[[\lua\bin]]
     },
-    path = { --path to add to the env variable, so that for example lua54.exe would be added to the path and be able to be called from any directories
+    path = {
       [[C:\Users\justi\AppData\lua\bin]]
     },
-    registry = { --creates new keys in the registry, such as the LUAROCKS_CONFIG which would point to your config.lua in question
+    registry = {
       { "LUA_LIBDIR",  AppData..[[\lua\bin]] }
     },
   },
   lua_inc = {
     name = "lua_inc.zip",
-    url = "https://master.dl.sourceforge.net/project/luabinaries/5.1.4/Docs%20and%20Sources/lua5_1_4_Sources.zip?viasf=1",
+    url = "https://master.dl.sourceforge.net/project/luabinaries/5.1.5/Docs%20and%20Sources/lua-5.1.5_Sources.zip?viasf=1",
     unzip = true,
     directories = {
       AppData..[[\lua]],
@@ -40,6 +39,7 @@ local files = {
     },
     path = {
       AppData..[[\lua\bin]],
+      
     },
     registry = {
       { "LUA_INCDIR",  AppData..[[\lua\inc]] }
@@ -53,11 +53,16 @@ local files = {
       { "luarocks-3.10.0-windows-64/luarocks.exe",        AppData..[[\luarocks\luarocks.exe]],       false },
       { "luarocks-3.10.0-windows-64/luarocks-admin.exe", AppData..[[\luarocks\luarocks-admin.exe]], false }
     },
-    registry = {
-      { "LUAROCKS_CONFIG",AppData..[[\luarocks\config.lua]] }
-    },
     directories = {
       AppData..[[\luarocks\]],
+      AppData..[[\luarocks\bin]]
+    },
+    registry = {
+      { "LUAROCKS_CONFIG",AppData..[[\luarocks\config.lua]] },
+      { "LUA_CPATH", AppData..[[\luarocks\share\lua\5.1\?.lua]]}
+    },
+    path = {
+      AppData..[[\luarocks\bin]]
     }
   }
 }
